8619b2f1bddcfae95c5daf07b1c6bd539409da80
bartu <bartucz.anna.0502@gamil.com>
2024-03-04 16:58:54 +0100

    new visitors

--- subprojects/frontends/demo-frontend/src/main/java/hu/bme/mit/theta/frontend/grammar/topVisitors.kt
+++ subprojects/frontends/demo-frontend/src/main/java/hu/bme/mit/theta/frontend/grammar/topVisitors.kt
@@ -35,6 +35,60 @@ class ModelVisitor : DemoBaseVisitor<String>() {
     override fun visitAssertion(ctx: DemoParser.AssertionContext?): String {
         return "assert " + ctx!!.comparison().text
     }
+
+    override fun visitVariableDeclaration(ctx: DemoParser.VariableDeclarationContext?): String {
+        val variableName = ctx!!.VarName().text
+        val variableType = ctx.type().accept(this)
+        return "val $variableName: $variableType"
+    }
+
+    override fun visitArithmeticExpression(ctx: DemoParser.ArithmeticExpressionContext?): String {
+        val leftOperand = ctx!!.leftOperand.accept(this)
+        val operator = ctx.operator.text
+        val rightOperand = ctx.rightOperand.accept(this)
+        return "($leftOperand $operator $rightOperand)"
+    }
+
+    override fun visitIfStatement(ctx: DemoParser.IfStatementContext?): String {
+        val condition = ctx!!.condition().accept(this)
+        val thenBranch = ctx.thenBranch().accept(this)
+        val elseBranch = ctx.elseBranch()?.accept(this) ?: ""
+        return "if ($condition) {\n$thenBranch\n} else {\n$elseBranch\n}"
+    }
+
+    override fun visitLogicalExpression(ctx: DemoParser.LogicalExpressionContext?): String {
+        val leftOperand = ctx!!.leftOperand.accept(this)
+        val operator = ctx.operator.text
+        val rightOperand = ctx.rightOperand.accept(this)
+        return "($leftOperand $operator $rightOperand)"
+    }
+
+    override fun visitLoop(ctx: DemoParser.LoopContext?): String {
+        val condition = ctx!!.condition().accept(this)
+        val body = ctx.body().accept(this)
+        return "while ($condition) {\n$body\n}"
+    }
+
+    override fun visitBreakStatement(ctx: DemoParser.BreakStatementContext?): String {
+        return "break"
+    }
+
+    override fun visitContinueStatement(ctx: DemoParser.ContinueStatementContext?): String {
+        return "continue"
+    }
+
+    override fun visitFunctionDeclaration(ctx: DemoParser.FunctionDeclarationContext?): String {
+        val functionName = ctx!!.functionName().text
+        val parameters = ctx.parameters().accept(this)
+        val body = ctx.body().accept(this)
+        return "fun $functionName($parameters) {\n$body\n}"
+    }
+
+    override fun visitFunctionCall(ctx: DemoParser.FunctionCallContext?): String {
+        val functionName = ctx!!.functionName().text
+        val arguments = ctx.arguments().accept(this)
+        return "$functionName($arguments)"
+    }
 }
 
 fun main() {
